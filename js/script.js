34f4edcb233ed6dd8104c57129d080147446585a
Vegard M <94170927+CodeVegard@users.noreply.github.com>
2022-02-21 14:36:48 +0100

    Polishing
    Removed dead code from script.js and polished the jackets.css somewhat (chagned bg opacity)
    
    Changed copy on checkout_succss and provided a link to the hero story of index.html

--- js/script.js
+++ js/script.js
@@ -12,14 +12,3 @@ console.log(getRandomInt());
 let random_number = "random_number"
 
 document.getElementById("random_number").innerHTML= getRandomInt()
-
-//--------------------------------------------------------------------//
-
-//I also want my site to be able to print the customers name and email when we get to the success page. This seems like it might be possible, but should not be my priority. Gotta focus on the task, namely the HTML and CSS. It seems possible, see links below:
-// https://stackoverflow.com/questions/14939010/get-value-from-text-area
-//https://gomakethings.com/getting-all-of-a-forms-elements-with-vanilla-js/
-//https://www.codegrepper.com/code-examples/javascript/how+to+get+value+of+input+vanilla+js 
-
-let name = getElementById("name").value;
-
-console.log(getElementById())
