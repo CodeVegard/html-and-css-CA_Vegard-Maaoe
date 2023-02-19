fda1659e7f1b8f233eaec8c7a6a30ad2328feabc
Vegard M <vm932648@gmail.com>
2023-02-19 11:22:02 +0100

    Stock status for product page
    Fixed the stock status for single products page + removed redundant css from jackets.css

--- js/code-for-CMS-CA/createProductPage.js
+++ js/code-for-CMS-CA/createProductPage.js
@@ -10,10 +10,28 @@ const productURL = `https://sellmo.no/Flower_Power/wp-json/wc/v3/products/0?id=$
 console.log(productURL);
 
 async function createProductPage() {
-    const productResponse = await fetch(productURL);
-    const productInfo = await productResponse.json();
+  const productResponse = await fetch(productURL);
+  const productInfo = await productResponse.json();
 
-    document.title = productInfo.name;
+  document.title = productInfo.name;
+  if (productInfo.stock_status === "inStock") {
+    productContainer.innerHTML = `
+    <div class="product_imgs">
+        <img class="big_img" src="${productInfo.images[0].src}" alt="${productInfo.images[0].alt}" title="${productInfo.images[0].name}" />
+      </div>
+
+      <section class="prod_text">
+        <h1 class="product_name">${productInfo.name}</h1>
+        ${productInfo.description}
+        <p class="price">${productInfo.price} kr</p>
+        <form class="buybox" action="checkout.html" method="GET">
+          <input type="submit" class="buy" name="${productInfo.id}" value="Buy Now" />
+          <input type="submit" class="chart" name="${productInfo.id}" value="Add to Chart" />
+        </form>
+      </section>
+    </div>
+    `;
+  } else {
     productContainer.innerHTML = `
     <div class="product_imgs">
         <img class="big_img" src="${productInfo.images[0].src}" alt="${productInfo.images[0].alt}" title="${productInfo.images[0].name}" />
@@ -25,7 +43,7 @@ async function createProductPage() {
         <p class="product_description">
         ${productInfo.description}
         </p>
-
+        <p class="not-in-stock">Not in Stock</p>
         <p class="price">${productInfo.price} kr</p>
 
         <form class="buybox" action="checkout.html" method="GET">
@@ -35,6 +53,8 @@ async function createProductPage() {
       </section>
     </div>
     `;
+  }
+
 }
 
 createProductPage();
