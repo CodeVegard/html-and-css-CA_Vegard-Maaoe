48b9277bf67ac162652e87b8819cddb18e2f7705
Vegard M <vm932648@gmail.com>
2023-02-19 12:43:16 +0100

    CSS'ing "Not in stock" info
    CSS'ing "Not in stock" info to look better on both mobile and desktop

--- js/code-for-CMS-CA/checkoutProduct.js
+++ js/code-for-CMS-CA/checkoutProduct.js
@@ -1,6 +1,7 @@
 const key = "ck_ce76707e29bca6491c1a2264e1e99177076b8e77";
 const secret = "cs_95db7727bef7886304f909ba0852363c0d80bacd";
 
+const checkoutBox = document.querySelector(".checkout");
 const productContainer = document.querySelector(".billing");
 const id = window.location.search.match(/[0-9]+/);
 console.log(id);
@@ -27,9 +28,9 @@ async function createCheckoutProduct() {
         <img src="${productInfo.images[0].src}" class="webshop_rowpic" alt="The Jacket" title="The Jacket" />
         <h2 class="product_name">${productInfo.name}</h2>
         ${productInfo.short_description}
-        <p class="not-in-stock">Not in Stock. This item will ship as soon as possible, you will receive updates to your email when it is sent</p>
         <p class="product_description">${productInfo.price} kr</p>
   `;  
+  checkoutBox.innerHTML +=`<p class="not-in-stock">Not in Stock. This item will ship as soon as possible, you will receive updates to your email when it is sent</p>`;
     }
 }
 
