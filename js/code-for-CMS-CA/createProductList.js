8f61da1a14a978aba2500c4b6296a39231cf8902
Vegard M <vm932648@gmail.com>
2023-02-20 16:33:19 +0100

    Cleanup
    Cleaning up the code for creating the list of products.
    
    Instead of a whole heck of a lot of if statements, I've made a reusable function to create the HTML. It accepts the container and array as arguments and creates HTML in the container based on the array.
    
    The array is determined by whether or not the checkbox is checked

--- js/code-for-CMS-CA/createProductList.js
+++ js/code-for-CMS-CA/createProductList.js
@@ -6,6 +6,38 @@ const unauthorizedUrl = "https://sellmo.no/Flower_Power/wp-json/wc/v3/products?"
 
 const inStockBtn = document.querySelector("#in-stock")
 
+function createHTML(container, arr) {
+
+  for (let i = 0; i < arr.length; i++){
+    const item = arr[i];
+    console.log(item);
+  
+    if (item.stock_status !== "instock") {
+      container.innerHTML += `
+      <div class="picwlink">
+        <a href="product.html?id=${item.id}">
+          <img class="webshop_rowpic" src="${item.images[0].src}" alt="${item.images[0].alt}" title="${item.images[0].name}" />
+        </a>
+        <h3 class="feat_prod_head">${item.name}</h3>
+        ${item.short_description}
+        <p class="not-in-stock">Not in Stock</p>
+        <a class="cta_btn" href="product.html?id=${item.id}">Buy Now!</a>
+      </div>
+      `;
+    } else {
+      container.innerHTML += `
+      <div class="picwlink">
+        <a href="product.html?id=${item.id}">
+          <img class="webshop_rowpic" src="${item.images[0].src}" alt="${item.images[0].alt}" title="${item.images[0].name}" />
+        </a>
+        <h3 class="feat_prod_head">${item.name}</h3>
+        ${item.short_description}
+        <a class="cta_btn" href="product.html?id=${item.id}">Buy Now!</a>
+      </div>
+      `;
+    }
+  }
+}
 
 async function createAllProducts() {
   const fullUrl = `${unauthorizedUrl}consumer_key=${key}&consumer_secret=${secret}`;
@@ -16,87 +48,19 @@ async function createAllProducts() {
   mensItemsContainer.classList.remove("loader");
   womensItemsContainer.classList.remove("loader");
 
+  let mensArr = finishedResponse.filter(arr => arr.tags[0].name === "men")
+  let womensArr = finishedResponse.filter(arr => arr.tags[0].name === "women");
+
+  if (inStockBtn.checked === true) {
+    mensArr = mensArr.filter(arr => arr.stock_status === "instock");
+    womensArr = womensArr.filter(arr => arr.stock_status === "instock");
+  };
+
   mensItemsContainer.innerHTML = "";
   womensItemsContainer.innerHTML = "";
 
-  for (let i = 0; i < finishedResponse.length; i++) {
-    const product = finishedResponse[i];
-
-    if (product.stock_status !== "instock" && product.tags[0].name === "men" && inStockBtn.checked === false) {
-      mensItemsContainer.innerHTML += `
-          <div class="picwlink">
-            <a href="product.html?id=${product.id}">
-              <img class="webshop_rowpic" src="${product.images[0].src}" alt="${product.images[0].alt}" title="${product.images[0].name}" />
-            </a>
-            <h3 class="feat_prod_head">${product.name}</h3>
-            ${product.short_description}
-            <p class="not-in-stock">Not in Stock</p>
-            <a class="cta_btn" href="product.html?id=${product.id}">Buy Now!</a>
-          </div>
-          `;
-    } else
-    if (product.stock_status === "instock" && product.tags[0].name === "men" && inStockBtn.checked === false) {
-      mensItemsContainer.innerHTML += `
-          <div class="picwlink">
-            <a href="product.html?id=${product.id}">
-              <img class="webshop_rowpic" src="${product.images[0].src}" alt="${product.images[0].alt}" title="${product.images[0].name}" />
-            </a>
-            <h3 class="feat_prod_head">${product.name}</h3>
-            ${product.short_description}
-            <a class="cta_btn" href="product.html?id=${product.id}">Buy Now!</a>
-          </div>
-          `;
-    } else
-    if (product.stock_status === "instock" && product.tags[0].name === "men" && inStockBtn.checked === true) {
-      mensItemsContainer.innerHTML += `
-          <div class="picwlink">
-            <a href="product.html?id=${product.id}">
-              <img class="webshop_rowpic" src="${product.images[0].src}" alt="${product.images[0].alt}" title="${product.images[0].name}" />
-            </a>
-            <h3 class="feat_prod_head">${product.name}</h3>
-            ${product.short_description}
-            <a class="cta_btn" href="product.html?id=${product.id}">Buy Now!</a>
-          </div>
-          `;
-    } else
-    if (product.stock_status === "instock" && product.tags[0].name === "women" && inStockBtn.checked === false) {
-      womensItemsContainer.innerHTML += `
-          <div class="picwlink">
-            <a href="product.html?id=${product.id}">
-              <img class="webshop_rowpic" src="${product.images[0].src}" alt="${product.images[0].alt}" title="${product.images[0].name}" />
-            </a>
-            <h3 class="feat_prod_head">${product.name}</h3>
-            ${product.short_description}
-            <a class="cta_btn" href="product.html?id=${product.id}">Buy Now!</a>
-          </div>
-          `;
-    } else
-    if (product.stock_status !== "instock" && product.tags[0].name === "women" && inStockBtn.checked === false) {
-      womensItemsContainer.innerHTML += `
-          <div class="picwlink">
-            <a href="product.html?id=${product.id}">
-              <img class="webshop_rowpic" src="${product.images[0].src}" alt="${product.images[0].alt}" title="${product.images[0].name}" />
-            </a>
-            <h3 class="feat_prod_head">${product.name}</h3>
-            ${product.short_description}
-            <p class="not-in-stock">Not in Stock</p>
-            <a class="cta_btn" href="product.html?id=${product.id}">Buy Now!</a>
-          </div>
-          `;
-    } else
-    if (product.stock_status === "instock" && product.tags[0].name === "women" && inStockBtn.checked === true) {
-      womensItemsContainer.innerHTML += `
-          <div class="picwlink">
-            <a href="product.html?id=${product.id}">
-              <img class="webshop_rowpic" src="${product.images[0].src}" alt="${product.images[0].alt}" title="${product.images[0].name}" />
-            </a>
-            <h3 class="feat_prod_head">${product.name}</h3>
-            ${product.short_description}
-            <a class="cta_btn" href="product.html?id=${product.id}">Buy Now!</a>
-          </div>
-          `;
-    }
-  }
+  createHTML(mensItemsContainer, mensArr);
+  createHTML(womensItemsContainer, womensArr);
 };
 
 setTimeout(() => {
