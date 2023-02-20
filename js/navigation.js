0b640ac6106581e2ecb8f022a24c68c61b0d0ea4
Vegard M <vm932648@gmail.com>
2023-02-20 13:01:34 +0100

    minimal change
    Removed  a comment from index, a console log from navigation.js and edited a note slightly

--- js/navigation.js
+++ js/navigation.js
@@ -6,7 +6,6 @@ const navDOM = document.querySelector(".nav_ul");
 function createNavigation(container, items) {
   for (let i = 0; i < items.length; i++) {
     const element = items[i];
-    console.log(element);
 
     container.innerHTML += `
           <li class="nav_li"><a href="${items[i].url}">${items[i].name}</a></li>
